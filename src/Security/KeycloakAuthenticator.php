96a157015e2572cc8252880e64bfc1a1dfa4290f
yuvem10 <matjuve33@icloud.com>
2023-11-23 17:17:37 +0100

    encore la meme erreur

--- src/Security/KeycloakAuthenticator.php
+++ src/Security/KeycloakAuthenticator.php
@@ -1,7 +1,7 @@
 <?php
+
 namespace App\Security;
 
-use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
 use KnpU\OAuth2ClientBundle\Security\Authenticator\OAuth2Authenticator;
@@ -11,18 +11,20 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\User;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
 
+
 /**
  * Description of KeycloakAuthenticator
  *
- * @author emds
+ * @author Lenovo T560
  */
 class KeycloakAuthenticator extends OAuth2Authenticator implements AuthenticationEntryPointInterface {
-
+    
     private $clientRegistry;
     private $entityManager;
     private $router;
@@ -33,14 +35,17 @@ class KeycloakAuthenticator extends OAuth2Authenticator implements Authenticatio
         $this->entityManager = $entityManager;
         $this->router = $router;        
     }
-
+    
     public function authenticate(Request $request): Passport {
         $client = $this->clientRegistry->getClient('keycloak');
+
         $accessToken = $this->fetchAccessToken($client);
+ 
         return new SelfValidatingPassport(
             new UserBadge($accessToken->getToken(), function() use ($accessToken, $client){
                 /** @var KeycloakUser $keycloakUser */
                 $keycloakUser = $client->fetchUserFromToken($accessToken);
+                
                 // 1) recherche du user dans la BDD à partir de son id Keycloak
                 $existingUser = $this->entityManager
                         ->getRepository(User::class)
@@ -71,18 +76,15 @@ class KeycloakAuthenticator extends OAuth2Authenticator implements Authenticatio
                 return $user;
             })
         );
-
     }
 
-    public function onAuthenticationFailure(Request $request, 
-            AuthenticationException $exception): ?Response {
-        $message = strtr($exception->getMessageKey(), $exception->getMessageData());
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response {
+          $message = strtr($exception->getMessageKey(), $exception->getMessageData());
         return new Response($message, Response::HTTP_FORBIDDEN);
     }
 
-    public function onAuthenticationSuccess(Request $request, 
-            TokenInterface $token, string $firewallName): ?Response {
-        $targetUrl = $this->router->generate('admin.voyages');
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response {
+        $targetUrl = $this->router->generate('admin');
         return new RedirectResponse($targetUrl);
     }
 
@@ -94,7 +96,6 @@ class KeycloakAuthenticator extends OAuth2Authenticator implements Authenticatio
     }
 
     public function supports(Request $request): ?bool {
-        return $request->attributes->get('_route') === 'oauth_check';
+         return $request->attributes->get('_route') === 'oauth_check';
     }
-
 }
